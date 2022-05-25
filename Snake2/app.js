0a5ca38fd0198a2a868482b53aee6587eca1b629
LucaSk-x <lucapiccinno64@gmail.com>
2022-05-25 17:01:46 +0200

    completed

--- Snake2/app.js
+++ Snake2/app.js
@@ -4,13 +4,16 @@ let numeroUtente;
 
 let arrayN = [];
 
+
+
 btn.addEventListener ("click", function() {
 
     while (numeroUtente === undefined) {
         let userInput = prompt("Inserisci un numero tra 5 e 10.");
         let userNumber = parseInt(userInput);
 
-        let arrayN = [];
+        
+        
     
     
         if (userNumber >= 5 && userNumber <= 10) {
@@ -19,14 +22,28 @@ btn.addEventListener ("click", function() {
             alert('inserisci un numero compreso tra 5 e 10')
         }
 
+        // faccio n volte
         for (let i = 0; i < userNumber; i++) {
-
+            // creo un nuovo array
+            let arrayN = [];
+            // creo una stringa vuota
+            let testoArray = "";
+            
+        
             for (let j = 0; j < 10; j++) {
-                arrayN.push(Math.floor((Math.random() * 100) + 1));
+                // creo un numero random da 1 a 100
+                let randomNumber = Math.floor((Math.random() * 100) + 1);
+                // pusho il numero random dentro arrayN
+                arrayN.push(randomNumber)
+                // aggiungo all fine della stringa il numero random
+                testoArray = testoArray + randomNumber + ", "
+
             }
+            
+            console.log(arrayN.join(", "));
+
         }
-        console.log(arrayN)
-        console.log()
+        
     }
 
     
